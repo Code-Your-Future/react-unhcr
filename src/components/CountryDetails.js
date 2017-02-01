bc34ee71f59ab6a1d308b08ed4b9c0a8ef865c69
sentayhuJo <sentayhu.alim@mail.com>
2017-02-01 23:51:30 +0000

    sum bugs fix

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -6,20 +6,19 @@ class CountryDetails extends React.Component{
 	render(){
 
 		const demographic=this.props.CountryDetail;	
-			
+		const sum=(numberFemale,numberMale)=>{
+			return parseInt(numberFemale) + parseInt(numberMale);
+		}
 		return(
 			<div className="app-country-statistics">
 				<strong>Country: </strong>{demographic.country_of_residence_en}<br/>
 				<strong>Year: </strong>{demographic.year}<br/>
 				<strong>Female Refugees: </strong>{demographic.female_total_value}<br/>
 				<strong>Male Refugees: </strong>{demographic.male_total_value}<br/>
-				<strong>Total: </strong>{this.sum(demographic.female_total_value,demographic.male_total_value)}
+				<strong>Total: </strong>{sum(demographic.female_total_value,demographic.male_total_value)}
 			</div>
 		);
-
-		sum=(numberFemale,numberMale)=>{
-			return parseInt(numberFemale) + parseInt(numberMale);
-		};
+	
 	}
 
 }
